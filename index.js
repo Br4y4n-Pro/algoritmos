f2989ae1a3022c9fe96f6e95deedcfc7179585ee
Br4y4n-Pro <82912260+Br4y4n-Pro@users.noreply.github.com>
2023-06-23 08:10:42 -0500

    estructura basica

---
+++ index.js
@@ -0,0 +1,22 @@
+//Pregunta: Escribe un algoritmo que tome dos números y devuelva su suma.
+    
+
+//Pregunta: Escribe un algoritmo que tome un número y devuelva "Es par" si es divisible por 2, y "Es impar" si no lo es.
+
+//Pregunta: Escribe un algoritmo que tome un arreglo de números y devuelva el número más grande.
+
+// Pregunta: Escribe un algoritmo que tome un string y devuelva la misma cadena pero con todas las letras en mayúsculas.
+
+// Pregunta: Escribe un algoritmo que tome un arreglo de strings y devuelva la cantidad de elementos que tienen una longitud mayor a 5.
+
+// Pregunta: Escribe un algoritmo que tome un número y devuelva la secuencia de Fibonacci hasta ese número.
+
+// Pregunta: Escribe un algoritmo que tome un arreglo de números y devuelva un nuevo arreglo con los elementos ordenados de menor a mayor.
+
+// Pregunta: Escribe un algoritmo que tome un número y devuelva true si es un número primo, y false si no lo es.
+
+// Pregunta: Escribe un algoritmo que tome una cadena de palabras separadas por espacios y devuelva la cadena con las palabras en orden inverso.
+
+// Pregunta: Escribe un algoritmo que tome un número y devuelva la suma de todos los números naturales hasta ese número.
+
+// Recuerda que estos son solo ejemplos de preguntas de ejercicios de algoritmos en JavaScript. Puedes intentar resolverlos por tu cuenta y luego buscar las soluciones o pedirlas si necesitas ayuda. ¡Buena suerte en tu práctica!
